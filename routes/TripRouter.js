1ed6cb43410014b8717222417fa05fac43751407
Russell Ochoa <ochoa.russell@gmail.com>
2022-09-03 18:44:42 -0500

    commented out route files to test server from AppRouter.js

--- routes/TripRouter.js
+++ routes/TripRouter.js
@@ -1,2 +1,2 @@
-const Router = require('express').Router()
-const middleware = require('../middleware')
+// const Router = require('express').Router()
+// const middleware = require('../middleware')
