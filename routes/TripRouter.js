06ff4e4c4db26abc5ba4eee52dfe8fc55329f945
Russell Ochoa <ochoa.russell@gmail.com>
2022-09-06 08:42:48 -0500

    add get route to TripRouter.js

--- routes/TripRouter.js
+++ routes/TripRouter.js
@@ -1,4 +1,7 @@
 const Router = require('express').Router()
 const middleware = require('../middleware')
+const controller = require('../controllers/TripController')
+
+Router.get('/', controller.GetAllTrips)
 
 module.exports = Router
