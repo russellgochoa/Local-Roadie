c226616575b7da2a80ffca92e3561492dea2ec22
Russell Ochoa <ochoa.russell@gmail.com>
2022-09-06 15:02:48 -0500

    create User seed

---
+++ seeders/20220906195515-users.js
@@ -0,0 +1,37 @@
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('users', [
+      {
+        firstName: 'Russell',
+        lastName: 'Ochoa',
+        username: 'russgus',
+        password: 'fxfxfxfx',
+        emai: 'russ@russ.com',
+        gear: 'Fender Deluxe Reverb, Epiphone Casino',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('users', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+}
