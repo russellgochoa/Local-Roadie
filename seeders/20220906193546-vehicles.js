47c78be32c5d3f4fdfb31786199624f5e969b58c
Russell Ochoa <ochoa.russell@gmail.com>
2022-09-06 14:51:28 -0500

    replace integer with string / vehicle-price

---
+++ seeders/20220906193546-vehicles.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const trip = require('../models/trip')
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('vehicles', [
+      {
+        type: 'van',
+        make: 'Honda',
+        model: 'Odyssey',
+        capacity: '10 passenger',
+        price: 20,
+        tripId: trip.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('vehicles', null, {})
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+}
